772ba51b9715a13dc14a4b5c501be2e0c6f54441
gksusha <gauravkumarsingh885300@gmail.com>
2026-02-17 04:31:40 +0530

    Created homepage layout

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 import list from "../../public/list.json";
+import Cards from "./Cards";
 
 function Freebook() {
   const filterData = list.filter((data) => data.category === "free");
@@ -57,30 +58,9 @@ function Freebook() {
         </div>
         <div>
           <Slider {...settings}>
-            <div>
-              <h3>1</h3>
-            </div>
-            <div>
-              <h3>2</h3>
-            </div>
-            <div>
-              <h3>3</h3>
-            </div>
-            <div>
-              <h3>4</h3>
-            </div>
-            <div>
-              <h3>5</h3>
-            </div>
-            <div>
-              <h3>6</h3>
-            </div>
-            <div>
-              <h3>7</h3>
-            </div>
-            <div>
-              <h3>8</h3>
-            </div>
+            {filterData.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
           </Slider>
         </div>
       </div>
