f14cdc1217238ac257f1cd8fe2b5bb45727e2e8d
gksusha <gauravkumarsingh885300@gmail.com>
2026-02-17 21:32:49 +0530

    Implemented signup functionality

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
@@ -12,14 +13,43 @@ function Login() {
             </button>
           </form>
           <h3 className="font-bold text-lg">Login</h3>
-            <div>
-                <span>
-                    Email
-                </span>
-                <br />
-                <input type="email" 
-                placeholder="Enter your email" className="w-80 px-3 border rounded-md outline none" />
+          {/* email */}
+          <div className="mt-4 space-y-2">
+            <span>Email</span>
+            <br />
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="w-80 px-3 py-1 border rounded-md outline none"
+            />
+          </div>
+          {/* password */}
+          <div className="mt-4 space-y-2">
+            <span>Password</span>
+            <br />
+            <input
+              type="text"
+              placeholder="Enter your password"
+              className="w-80 px-3 py-1 border rounded-md outline none"
+            />
+            {/* login button */}
+
+            <div className="flex justify-around mt-4">
+              {" "}
+              <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
+                Login
+              </button>
+              <p>
+                Not registered?{" "}
+                <Link
+                  to="/signup"
+                  className="underline text-blue-500 cursor-pointer"
+                >
+                  Signup
+                </Link>{" "}
+              </p>
             </div>
+          </div>
         </div>
       </dialog>
     </div>
