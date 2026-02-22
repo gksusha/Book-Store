0f93ccb9067b4a4418d55e97dcd54c861cca8086
gksusha <gauravkumarsingh885300@gmail.com>
2026-02-23 01:43:40 +0530

    Completed backend API with authentication and database integration

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,21 +1,53 @@
 import User from "../model/user.model.js";
+import bcryptjs from "bcryptjs";
 
-export const signup = (req, res) => {
+export const signup = async (req, res) => {
   try {
     const { fullname, email, password } = req.body;
-    const user = user.findOne({ email });
+    const user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({ message: "User already exists" });
     }
+    const hashedPassword = await bcryptjs.hash(password, 10);
     const createdUser = new User({
-      fullname,
-      email,
-      password,
+      fullname: fullname,
+      email: email,
+      password: hashedPassword,
     });
-    createdUser.save();
-    res.status(201).json({ message: "User created successfully" });
+    await createdUser.save();
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        _id: createdUser._id,
+        fullname: createdUser.fullname,
+        email: createdUser.email,
+      },
+    });
   } catch (error) {
     console.log("Error signing up: " + error.message);
     res.status(500).json({ message: "Error signing up" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!user || !isMatch) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    } else {
+      res.status(200).json({
+        message: "Login successful",
+        user: {
+          _id: user._id,
+          fullname: user.fullname,
+          email: user.email,
+        },
+      });
+    }
+  } catch (error) {
+    console.log("Error logging in: " + error.message);
+    res.status(500).json({ message: "Error logging in" });
   }
 };
