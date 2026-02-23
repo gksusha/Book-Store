320f34e924fcff824de5e8c0acaa5b9b061a8462
gksusha <gauravkumarsingh885300@gmail.com>
2026-02-23 15:51:15 +0530

    improved ui

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -4,14 +4,14 @@ import banner from "../../public/Banner.jpg";
 function Banner() {
   return (
     <>
-      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-20">
-        <div className="w-full order-2 md:order-1 md:w-1/2 mt-12 md:mt-32">
-          <div className="space-y-12">
-            <h1 className="text-4xl font-bold">
-              Hello, Welcome here to learn something{" "}
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-10">
+        <div className="w-full order-2 md:order-1 md:w-1/2 mt-12 md:mt-36">
+          <div className="space-y-8">
+            <h1 className="text-2xl md:text-4xl font-bold">
+              Hello, welcomes here to learn something{" "}
               <span className="text-pink-500"> new everyday!!!</span>
             </h1>
-            <p className="text-xl">
+            <p className="text-sm md:text-xl">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
               sit molestiae optio eveniet fugiat dicta expedita? Sunt,
               blanditiis optio numquam tempora voluptate quod amet perspiciatis
@@ -33,7 +33,11 @@ function Banner() {
           <button className="mt-6 btn btn-secondary">Secondary</button>
         </div>
         <div className="order-1 w-full md:w-1/2">
-          <img src={banner} className="w-90 h-90" alt="" />
+          <img
+            src={banner}
+            className="md:w-[550px] md:h-[460px] md:ml-12"
+            alt=""
+          />
         </div>
       </div>
     </>
--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Cards({ item }) {
   return (
     <>
-      <div className="mt-4 my-3">
-        <div className="card w-92 bg-base-100 w-96 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
+      <div className="mt-4 my-3 p-3">
+        <div className="card w-92 bg-base-100 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
           <figure>
-            <img src={item.image} alt="Shoes" />
+            <img src={item.image} alt="Books" />
           </figure>
           <div className="card-body">
             <h2 className="card-title">
@@ -16,7 +16,9 @@ function Cards({ item }) {
             <p>{item.title}</p>
             <div className="card-actions justify-between">
               <div className="badge badge-outline">${item.price}</div>
-              <div className="cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-pink-500 hover:text-white duration-200">Buy Now</div>
+              <div className="cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-pink-500 hover:text-white duration-200">
+                Buy Now
+              </div>
             </div>
           </div>
         </div>
